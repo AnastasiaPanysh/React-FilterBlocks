bd1ce97a0327c77bc276e52fbc3ecad2d0772c64
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-05-18 13:39:25 +0300

    add Pagination

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,18 +1,39 @@
+import { useState, useRef } from "react";
+import { Pagination } from "@mantine/core";
 import style from "./style.module.scss";
 import storage from "../../Storage/Storage.json";
 import Item from "./Item";
 
 function Content() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSizeRef = useRef(4);
+
+  const paginatedList = storage.slice(
+    (currentPage - 1) * pageSizeRef.current,
+    currentPage * pageSizeRef.current
+  );
+
+ function PageChange(page) {
+    setCurrentPage(page);
+  };
+
   return (
     <>
-      {storage.map((item) => (
+      {paginatedList.map((item) => (
         <Item
+          key={item.id}
           vacancy={item.vacancy}
           salary={item.salary}
           workday={item.workday}
           city={item.city}
-        ></Item>
+        />
       ))}
+      <Pagination
+        total={Math.ceil(storage.length / pageSizeRef.current)}
+        value={currentPage}
+        onChange={PageChange}
+        position="center"
+      />
     </>
   );
 }
